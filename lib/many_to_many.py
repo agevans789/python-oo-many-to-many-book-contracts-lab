708f4ab10f3fab2aec2fe271d36bca5d4b4822ef
Ann Grace Evans <agevans789@gmail.com>
2026-02-19 17:23:42 -0500

    2 passed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -3,8 +3,17 @@ class Author:
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.authors = []  # List of Author instances
 
+    def contracts(self):
+        # This method would return a list of Contract instances associated with this book
+        return [Contract() for _ in self.authors]  # Placeholder implementation
+    
+    def author_names(self):
+        # This method would return a list of author names for this book
+        return [author.name for author in self.authors]  # Placeholder implementation
 
 class Contract:
     pass
