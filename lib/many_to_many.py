b7f3a526fdd985067fe45799b9e8dcba95344efd
Ann Grace Evans <agevans789@gmail.com>
2026-02-19 17:33:46 -0500

    6 passed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,19 +1,22 @@
 class Author:
-    pass
-
+    all = []  # Class-level list to keep track of all Author instances
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)  # Add this instance to the class-level list of all authors
 
 class Book:
+    all = []  # Class-level list to keep track of all Book instances
     def __init__(self, title):
         self.title = title
-        self.authors = []  # List of Author instances
+        Book.all.append(self)  # Add this instance to the class-level list of all books
 
     def contracts(self):
         # This method would return a list of Contract instances associated with this book
-        return [Contract() for _ in self.authors]  # Placeholder implementation
+        return [Contract() for _ in self.all_titles]  # Placeholder implementation
     
-    def author_names(self):
-        # This method would return a list of author names for this book
-        return [author.name for author in self.authors]  # Placeholder implementation
+    def authors(self):
+        # This method would return a list of author names for this book using Contract class as intermediary
+        return [author.name for author in self.all_authors]  # Placeholder implementation
 
 class Contract:
     pass
